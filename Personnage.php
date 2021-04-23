f7f226c9df3516d85dda538f73bf33ab813d52d6
RobinPr10 <robin.prudhomme8@gmail.com>
2021-04-23 12:51:15 +0200

    mapping

--- Personnage.php
+++ Personnage.php
@@ -2,16 +2,22 @@
 
  class Personnage{
 
-
+   private $id;
     private $nom;
     private $prenom;
-    
-      public function setNom($newNom){
-       $this->nom=$newNom;
-    }
 
-    public function setPrenom($newPrenom){
-        $this->prenom=$newPrenom;
+    public function _construct($id,$pdo){
+        $req="select * from Personnage where id=".id;
+        echo $req; ; 
+        $RequetStatement=$pdo->query($req);
+        if($RequetStatement){
+           $tab=$RequetStatement->fetch();
+           $this->id=$tab["nom"];
+           $this->id=$tab["Prenom"]
+        }
+    
+        $this->nom=$tab["nom"];
+        $this->prenom=$tab["Prenom"]
      }
     public function Presentetoi(){
         echo"Je suis".$this->prenom." ".$this->nom;
--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
   echo"teste";
 include "Personnage.php";
+$pdo=new PDO("")
 
-$Perso1=new Personnage();
-$Perso2=new Personnage();
+$Perso1=new Personnage(1,$pdo);
+$Perso2=new Personnage(2,$pdo);
 
 $Perso1->setNom("Thomas");
 $Perso1->Presentetoi( );
+$Perso2->Presentetoi( );
 
 ?>
